6e662540ba3dbf1386576a7e44e12627f0d35128
Mohd Shadab <ms.shadab06@gmail.com>
2024-10-17 12:52:19 +0530

    stats changed

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -4,21 +4,21 @@ import CountUp from "react-countup";
 
 const stats = [
   {
-    num: 12,
+    num: 2,
     text: "Years of experience",
   },
   {
-    num: 26,
+    num: 5,
     text: "Projects completed",
   },
   {
     num: 8,
     text: "Technologies mastered",
   },
-  {
-    num: 500,
-    text: "Code commits",
-  },
+  // {
+  //   num: 500,
+  //   text: "Code commits",
+  // },
 ];
 
 const Stats = () => {
